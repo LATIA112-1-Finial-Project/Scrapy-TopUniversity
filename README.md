74e94c8d9be227a046b74cba9ab75f773e2ffd55
KutsunaSubaRya <index20010928@gmail.com>
2023-11-15 17:15:07 +0800

    feat: init top university codebase

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Scrapy-TopUniversity
+# LATIA112-1 HW02
+
+## Virtual Environment
+
+Recommend to use venv to isolate your environment.
+
+```bash
+$ python3.9 -m venv venv
+$ source venv/bin/activate
+```
+
+## Dependancy
+
+* Requirements.txt
+
+  * ```bash
+    $ pip install -r requirements.txt
+    ```
+* Chromedriver for MacOS
+  * ```bash
+    $ brew install --cask chromedriver
+    ```
+
+## How To Use My Scrapy Code
+
+1. Install the dependancies
+2. Note that `file.csv` already exist in codebase, hence you **MAY CHANGE THE NAME TO THE OTHER** so as not to keep writing the data into `file.csv`. 
+    ```bash
+    $ cd Scrapy_topUniversity
+    $ scrapy crawl topUniversity -o file.csv
+    ```
+
+In my code, I am dealing with a Dynamic JS Website, where the data in the table is loaded dynamically. To retrieve the corresponding data, I have opted to use the website's API endpoint. 
+
+The method to obtain the endpoint involves opening the Network tab, selecting the Fetch/XHR filter tag, refreshing the webpage, and identifying the relevant keyword in the name file associated with the endpoint. By choosing the header, the request URL, which is the desired API endpoint, can be found. This approach allows for the successful retrieval of data in JSON content type.
+
+## Demo Video
+
+* [HW02 - Scrapy_topUniversity](https://youtu.be/BhOQm14mPn4)
+
+## Material
+
+* [QS World University Rankings 2024: Top global universities](https://www.topuniversities.com/university-rankings/world-university-rankings/2024)
---
+++ Scrapy_topUniversity/topUniversity/spiders/universities.py
@@ -0,0 +1,41 @@
+import scrapy
+
+
+class TopuniversitySpider(scrapy.Spider):
+    name = "topUniversity"
+    allowed_domains = ["www.topuniversities.com"]
+    start_urls = ["https://www.topuniversities.com/university-rankings/world-university-rankings/2024"]
+
+    def start_requests(self):
+        # I had write the reason why I use this url in the README.md
+        url = "https://www.topuniversities.com/rankings/endpoint?nid=3897789&page=0&items_per_page=1500&tab=indicators&region=&countries=&cities=&search=&star=&sort_by=rank&order_by=asc&program_type="
+        # Send a request to the url
+        yield scrapy.Request(url=url)
+    def parse(self, response):
+        score_nodes = response.json()["score_nodes"]
+        # Loop through the score_nodes
+        for university in score_nodes:
+            # Yield a dictionary and store the information that we want via I/O redirection. (See README.md)
+            yield {
+                "name": university["title"].strip(),
+                "overall rank": university["rank"],
+                "overall score": university["overall_score"],
+                "academic reputation rank": university["scores"][0]["rank"],
+                "academic reputation score": university["scores"][0]["score"],
+                "employer reputation rank": university["scores"][1]["rank"],
+                "employer reputation score": university["scores"][1]["score"],
+                "faculty student rank": university["scores"][2]["rank"],
+                "faculty student score": university["scores"][2]["score"],
+                "citations per faculty rank": university["scores"][3]["rank"],
+                "citations per faculty score": university["scores"][3]["score"],
+                "international faculty rank": university["scores"][4]["rank"],
+                "international faculty score": university["scores"][4]["score"],
+                "international students rank": university["scores"][5]["rank"],
+                "international students score": university["scores"][5]["score"],
+                "international Research Network rank": university["scores"][6]["rank"],
+                "international Research Network score": university["scores"][6]["score"],
+                "employment outcomes rank": university["scores"][7]["rank"],
+                "employment outcomes score": university["scores"][7]["score"],
+                "sustainability rank": university["scores"][8]["rank"],
+                "sustainability score": university["scores"][8]["score"],
+            }
